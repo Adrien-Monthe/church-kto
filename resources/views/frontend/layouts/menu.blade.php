e082f9c9f8fda764cd4b0d7acbc1a49ebe1d26b1
Steve Adrien <montheadrien@gmail.com>
2023-05-03 16:39:55 +0100

    Boot

--- resources/views/frontend/layouts/menu.blade.php
+++ resources/views/frontend/layouts/menu.blade.php
@@ -8,23 +8,55 @@
             </li>
 
             <li>
-                <a href="#">Gallerie</a>
+                <a href="#">Actualités</a>
+                <ul>
+                    <li><a href="index.html" class="">Agenda</a></li>
+                    <li><a href="index-2.html">Vie de la diocèse</a></li>
+                    <li><a href="index-3.html">Vie des enfants</a></li>
+                    <li><a href="index-3.html">culture</a></li>
+                    <li><a href="index-3.html">évènements</a></li>
+                </ul>
 
             </li>
 
             <li class="{{ Route::currentRouteName() == "prayer" ? "active" : "" }}">
-                <a href=" {{ route('prayer') }}">Prières</a>
+                <a href="#">Questions de foi</a>
+
+                <ul>
+                    <li><a href="index.html" class="">Questions réponses</a></li>
+                    <li><a href="index-2.html">Textes du jour</a></li>
+                    <li class="{{ Route::currentRouteName() == "prayer" ? "active" : "" }}"><a href="{{ route('prayer') }}">Prières</a></li>
+                    <li><a href="index-3.html">Réflexions et catéchèses</a></li>
+
+                </ul>
 
             </li>
 
             <li>
-                <a href="#">Blog</a>
+                <a href="#">Amour & Sexualité</a>
+
+                <ul>
+                    <li><a href="index.html" class="">Témoignages, </a></li>
+                    <li><a href="index-2.html">Réponses aux questions</a></li>
+                </ul>
 
             </li>
+
             <li>
-                <a href="#">Boutique</a>
+                <a href="#">Ktosphère</a>
+
+                <ul>
+                    <li><a href="index.html" class="">Galeries</a></li>
+                    <li><a href="index-2.html">Vidéos</a></li>
+                    <li><a href="index-2.html">Groupes</a></li>
+                    <li><a href="index-2.html">Mouvements</a></li>
+                    <li><a href="index-2.html">Liens</a></li>
+                </ul>
 
             </li>
+
+
+
             <li><a href="{{ route('contact') }}">Contactez Nous</a></li>
         </ul>
         <a href="cause-single.html" class="default-btn"><span>Faites un Don <i class="fas fa-heart"></i></span></a>
