9e2a3dc34aaa5446f7998bdc9fee8db8292c30f9
Steve Adrien <montheadrien@gmail.com>
2023-05-03 19:40:38 +0100

    Boot

--- app/Http/Requests/StorePtagRequest.php
+++ app/Http/Requests/StorePtagRequest.php
@@ -11,7 +11,7 @@ class StorePtagRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/UpdatePcommentRequest.php
+++ app/Http/Requests/UpdatePcommentRequest.php
@@ -11,7 +11,7 @@ class UpdatePcommentRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
